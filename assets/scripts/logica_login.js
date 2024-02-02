74108e5132789233956c0b5ff44bd0d0597d98ac
Informatica <informatica@alumno.local>
2024-02-02 11:09:42 -0600

    Funcionalidad

---
+++ assets/scripts/logica_login.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        var email = document.getElementById('emailLogin').value;
+        var password = document.getElementById('passLogin').value;
+
+        if (email === 'admin' && password === 'admin') {
+            window.location.href = 'assets/pages/contacts.html';
+        } else {
+            alert('Correo o contraseña incorrectos');
+        }
+    });
+});
